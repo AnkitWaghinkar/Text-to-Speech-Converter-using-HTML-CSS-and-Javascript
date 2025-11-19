5100657da78c65a684cbaa30cec26ea920019176
AnkitWaghinkar <138594474+AnkitWaghinkar@users.noreply.github.com>
2025-11-19 13:19:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    voiceSelect.innerHTML = '';
+
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+};
+
+voiceSelect.addEventListener("change", () => {
+    speech.voice = voices[voiceSelect.value];
+});
+
+document.querySelector("button").addEventListener("click", () => {
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech)
+});
+
+window.speechSynthesis.cancel();
+
+document.querySelector("#stop").addEventListener("click", () => {
+    window.speechSynthesis.cancel();
+});
